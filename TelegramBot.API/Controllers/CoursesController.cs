1eb29f1120313f3a4c6f7f3df0020f405c64adef
maybeeK <rom.bosyj@gmail.com>
2023-07-07 13:55:03 +0300

    Create controller for course management

---
+++ TelegramBot.API/Controllers/CoursesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TelegramBot.API.Services.Interfaces;
+
+namespace TelegramBot.API.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ICourseService _courseService;
+        public CoursesController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+    }
+}
