9388208ffc1c8729e875a027b32b37dbab6bf2e3
maybeeK <rom.bosyj@gmail.com>
2023-07-11 13:54:11 +0300

    Add DTO conversion class

---
+++ TelegramBot.API/Extensions/DtoConversions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TelegramBot.API.Entities;
+using TelegramBot.Shared.DTOs;
+
+namespace TelegramBot.API.Extensions
+{
+    public static class DtoConversions
+    {
+        public static CourseDto ConvertToDto(this Course course)
+        {
+            return new CourseDto
+            {
+                Id = course.Id,
+                Name = course.Name,
+                Description = course.Description,
+                Link = course.Link
+            };
+        }
+
+        public static IEnumerable<CourseDto> ConvertToDto(this IEnumerable<Course> courses)
+        {
+            return (from course in courses
+                   select new CourseDto { 
+                        Id = course.Id,
+                        Name = course.Name,
+                        Description = course.Description,
+                        Link = course.Link
+                   }).ToList();
+        }
+
+        public static IEnumerable<UserTagDto> ConvertToDto(this IEnumerable<UserTag> userTags)
+        {
+            return (from userTag in userTags
+                    select new UserTagDto
+                    {
+                        Id = userTag.Id,
+                        UsertId= userTag.UsertId,
+                        Tag = userTag.Tag
+                    }).ToList();
+        }
+    }
+}
