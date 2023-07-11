30f85b1cb724af44cb94cd73970353484304ab4d
maybeeK <rom.bosyj@gmail.com>
2023-07-11 13:57:36 +0300

    Add new service for user tags

---
+++ TelegramBot.API/Services/Interfaces/IUserTagService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TelegramBot.API.Entities;
+using TelegramBot.Shared.DTOs;
+
+namespace TelegramBot.API.Services.Interfaces
+{
+    public interface IUserTagService
+    {
+        Task<IEnumerable<UserTag>> GetTagsByUserId(long userId);
+        Task<IEnumerable<UserTag>> AddUserTags(IEnumerable<UserTagDto> userTags);
+        Task<IEnumerable<UserTag>> DeleteTagsByUserId(long userId);
+    }
+}
---
+++ TelegramBot.API/Services/UserTagService.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TelegramBot.API.Data;
+using TelegramBot.API.Entities;
+using TelegramBot.API.Services.Interfaces;
+using TelegramBot.Shared.DTOs;
+
+namespace TelegramBot.API.Services
+{
+    public class UserTagService : IUserTagService
+    {
+        private readonly AppDbContext _context;
+        public UserTagService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<UserTag>> AddUserTags(IEnumerable<UserTagDto> userTagsDto)
+        {
+            var userTags = from userTag in userTagsDto
+                           select new UserTag()
+                           {
+                               Id = userTag.Id,
+                               UsertId = userTag.UsertId,
+                               Tag = userTag.Tag
+                           };
+
+            await _context.UserTags.AddRangeAsync(userTags);
+
+            await _context.SaveChangesAsync();
+            
+            return userTags;
+        }
+
+        public async Task<IEnumerable<UserTag>> DeleteTagsByUserId(long userId)
+        {
+            var tagsToDelete = await GetTagsByUserId(userId);
+
+            if (tagsToDelete == null)
+            {
+                return default;
+            }
+
+            _context.UserTags.RemoveRange(tagsToDelete);
+
+            await _context.SaveChangesAsync();
+            
+            return tagsToDelete;
+        }
+
+        public async Task<IEnumerable<UserTag>> GetTagsByUserId(long userId)
+        {
+            return await _context.UserTags.Where(e=>e.UsertId==userId).ToListAsync();
+        }
+    }
+}
