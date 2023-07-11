a2fa72075a34ffd7c91c170a538acabfce621b35
maybeeK <rom.bosyj@gmail.com>
2023-07-11 13:53:44 +0300

    Add DTOs

---
+++ TelegramBot.Shared/DTOs/UserTagDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBot.Shared.DTOs
+{
+    public class UserTagDto
+    {
+        public int Id { get; set; }
+        public long UsertId { get; set; }
+        public string Tag { get; set; }
+    }
+}
