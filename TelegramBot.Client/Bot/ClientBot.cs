288811668f5a4d171dbc823b647e90c0d0f4cdf6
maybeeK <rom.bosyj@gmail.com>
2023-07-18 15:42:13 +0300

    Add bot, configuration

---
+++ TelegramBot.Client/Bot/ClientBot.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using TelegramBot.Client.CommandsFactory;
+using TelegramBot.Client.Extensions;
+
+namespace TelegramBot.Client.Bot
+{
+    public class ClientBot
+    {
+        private readonly string _key;
+        private CommandFactory _factory;
+        public ClientBot(string key)
+        {
+            _key = key;
+            _factory = new CommandFactory();
+        }
+
+        public async Task StartLoopAsync(CancellationToken _cancellationToken)
+        {
+            var bot = new TelegramBotClient(_key);
+            var user = await bot.GetMeAsync();
+            int offset = 0;
+
+            while (!_cancellationToken.IsCancellationRequested)
+            {
+                var updates = new Update[0];
+                try
+                {
+                    updates = await bot.GetUpdatesAsync(offset: offset);
+
+                }
+                catch (TaskCanceledException)
+                {
+                    
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("ERROR WHILE GETTIGN UPDATES - " + ex);
+                }
+
+                foreach (var update in updates)
+                {
+                    offset = update.Id + 1;
+                    ProcessUpdate(bot, update, user);
+                }
+
+                await Task.Delay(1000);
+            }
+        }
+
+        private async Task ProcessUpdate(TelegramBotClient bot, Update update, User user)
+        {
+            try
+            {
+                var gettingText = update.Message.Text;
+                
+                string stringCommand = string.Empty;
+                string body = string.Empty;
+                string replyText = string.Empty;
+
+                if (gettingText.HasCommand())
+                {
+                    stringCommand = gettingText.ExtractCommand();
+                    body = gettingText.ExtractBody(stringCommand);
+                }
+                else
+                {
+                    stringCommand = "NoExistingCommand";
+                }
+
+                var command = _factory.Create(stringCommand);
+
+                replyText = await command.Proccess(body: body, userId: update.Message.Chat.Id, parseMode: null);
+
+                await bot.SendTextMessageAsync(update.Message.Chat.Id, replyText, disableWebPagePreview: true);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"ERROR - {ex.Message}");
+                throw;
+            }
+        }
+    }
+}
---
+++ TelegramBot.Client/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using System.Net.Http.Json;
+using TelegramBot.Client.Bot;
+using TelegramBot.Shared.DTOs;
+
+namespace TelegramBot.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string key = configuration.GetConnectionString("TelegramKey");
+
+            ClientBot bot = new(key);
+
+            try
+            {
+                bot.StartLoopAsync(CancellationToken.None).Wait();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("MAIN LOOP EXIT ERROR - " + ex);
+                Thread.Sleep(30000);
+            }
+        }
+    }
+}
