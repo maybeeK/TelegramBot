2bdcff4ddc0b0eebb77ddf4105e1fa12ca25413d
maybeeK <rom.bosyj@gmail.com>
2023-07-20 16:38:37 +0300

    Add service for user tags

---
+++ TelegramBot.Client/Services/Intervaces/ITagService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TelegramBot.Shared.DTOs;
+
+namespace TelegramBot.Client.Services.Intervaces
+{
+    public interface ITagService : IDisposable
+    {
+        Task<bool> ClearUserTags(long userId);
+        Task<bool> AddUserTags(long userId, IEnumerable<UserTagDto> userTags);
+    }
+}
---
+++ TelegramBot.Client/Services/TagService.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using TelegramBot.Client.Services.Intervaces;
+using TelegramBot.Shared.DTOs;
+
+namespace TelegramBot.Client.Services
+{
+    public class TagService : ITagService
+    {
+        private const string BASE_URL = "https://localhost:7103/";
+        private const int MAX_TAGS_PER_USER = 1;
+
+        private readonly HttpClient _httpClient;
+        public TagService()
+        {
+            _httpClient = new HttpClient() { 
+                    BaseAddress = new Uri(BASE_URL)
+                };
+        }
+        public async Task<bool> AddUserTags(long userId, IEnumerable<UserTagDto> userTags)
+        {
+            try
+            {
+                int userTagsAmount = (await GetUserTags(userId)).Count();
+                if (userTagsAmount + userTags.Count() > MAX_TAGS_PER_USER)
+                {
+                    return false;
+                }
+
+                var responce = await _httpClient.PostAsJsonAsync($"api/UserTags", userTags);
+
+                if (!responce.IsSuccessStatusCode)
+                {
+                    return false;
+                }
+
+                return true;
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("SERVICE ERROR!!!");
+                throw;
+            }
+        }
+        private async Task<IEnumerable<UserTagDto>> GetUserTags(long userId)
+        {
+            try
+            {
+                var responce = await _httpClient.GetAsync($"api/UserTags/{userId}");
+
+                if (responce.IsSuccessStatusCode)
+                {
+                    if (responce.StatusCode == HttpStatusCode.NoContent)
+                    {
+                        return Enumerable.Empty<UserTagDto>();
+                    }
+
+                    return await responce.Content.ReadFromJsonAsync<IEnumerable<UserTagDto>>();
+                }
+                else 
+                {
+                    throw new Exception();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public async Task<bool> ClearUserTags(long userId)
+        {
+            try
+            {
+                var responce = await _httpClient.DeleteAsync($"api/UserTags/{userId}");
+
+                if (responce.IsSuccessStatusCode)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("SERVICE ERROR!!!");
+                throw;
+            }
+        }
+
+        public void Dispose()
+        {
+            _httpClient.Dispose();
+        }
+    }
+}
