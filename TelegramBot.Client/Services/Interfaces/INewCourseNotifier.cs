88be87451ae488e630cbba85f414281d08a923cf
maybeeK <rom.bosyj@gmail.com>
2023-07-24 13:43:31 +0300

    Create interfaces/service for notify about new courses

---
+++ TelegramBot.Client/Services/Interfaces/INewCourseNotifier.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TelegramBot.Shared.DTOs;
+
+namespace TelegramBot.Client.Services.Interfaces
+{
+    public interface INewCourseNotifier
+    {
+        Task Notify(IEnumerable<CourseDto> newCourses);
+    }
+}
---
+++ TelegramBot.Client/Services/NewCourseNotifier.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TelegramBot.Client.Services.Interfaces;
+using TelegramBot.Shared.DTOs;
+
+namespace TelegramBot.Client.Services
+{
+    public class NewCourseNotifier : INewCourseNotifier
+    {
+        public Task Notify(IEnumerable<CourseDto> newCourses) //TODO Implement interface
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
